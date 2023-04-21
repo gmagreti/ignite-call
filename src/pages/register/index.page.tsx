90c9b7032ec80a951ba4f7854eb96e42d0c30fca
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-04-21 15:43:04 -0300

    feat: Manipulando cookies no Next.js

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -8,6 +8,7 @@ import { z } from 'zod'
 
 import { Container, Form, FormError, Header } from './styles'
 import { api } from '~/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -49,7 +50,12 @@ export default function Register() {
         username: data.username,
       })
     } catch (err) {
-      console.log(err)
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
+      console.error(err)
     }
   }
 
