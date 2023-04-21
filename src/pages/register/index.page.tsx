6df674238b240a732b2bee4eaedb8848975cbb0c
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-04-21 15:28:38 -0300

    feat: Persistindo usuário no Prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
@@ -5,8 +7,7 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 import { Container, Form, FormError, Header } from './styles'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { api } from '~/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -41,8 +42,15 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
 
-  function handleRegister(data: RegisterFormData) {
-    console.log(data)
+  async function handleRegister(data: RegisterFormData) {
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
