87033e32acab776d349ebc9e7009476c2fa20f24
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-05-04 18:38:30 -0300

    feat: Navegando entre os meses

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { CaretLeft, CaretRight } from 'phosphor-react'
 import { getWeekDays } from '../../utils/get-week-days'
 import {
@@ -8,22 +9,43 @@ import {
   CalendarHeader,
   CalendarTitle,
 } from './styles'
+import { useState } from 'react'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
+  function handleNextMonth() {
+    const nextMonthDate = currentDate.add(1, 'month')
+
+    setCurrentDate(nextMonthDate)
+  }
+
+  const currentMonth = currentDate.format('MMMM')
+
+  const currentYear = currentDate.format('YYYY')
+
   const shortWeekDays = getWeekDays({ short: true })
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Dezembro <span>2022</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next month">
             <CaretRight />
           </button>
         </CalendarActions>
