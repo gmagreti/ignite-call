6f1b153850d3a44ca3c84e4b0a492eede8a02570
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-05-08 14:52:07 -0300

    feat: Envolvendo provider do react query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,10 @@
-import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+import { QueryClientProvider } from '@tanstack/react-query'
+
 import { globalStyles } from '~/styles/global'
-import '../lib/dayjs'
+import '~/lib/dayjs'
+import { queryClient } from '~/lib/react-query'
 
 globalStyles()
 
@@ -10,8 +13,10 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
