1002a0e643865785d295eb712725073084b3d968
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-05-12 18:30:23 -0300

    feat:  Finalizando query de dias lotados

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -26,6 +26,7 @@ type CalendarWeeks = CalendarWeek[]
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 interface CalendarProps {
@@ -60,12 +61,12 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   const username = String(router.query.username)
 
   const { data: blockedDates } = useQuery<BlockedDates>(
-    ['blocked-dates', currentDate.get('year'), currentDate.get('month')],
+    ['blocked-dates', currentDate.get('year'), currentDate.get('month') + 1],
     async () => {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month'),
+          month: currentDate.get('month') + 1,
         },
       })
 
@@ -114,7 +115,8 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
